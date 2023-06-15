6962455e05cb1ecbc71cfcf18f543ee528080bd9
Alberto <Alberto@DESKTOP-T3SP101>
2023-06-15 18:46:20 +0100

    Agregar archivos de proyecto.

---
+++ Portfolio 2/header_functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+double average(double num1, double num2);
+int tenth_power(int num);
+bool is_palindrome(std::string text);
+void pizzaParty();
---
+++ Portfolio 2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "header_functions.h"
+
+
+int main() {
+
+	std::cout << is_palindrome("noon") << "\n";
+	std::cout << tenth_power(4) << "\n";
+	std::cout << average(4.0, 7.0) << "\n";
+	pizzaParty();
+}
